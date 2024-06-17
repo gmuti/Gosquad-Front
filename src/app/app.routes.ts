68e22710b6df1228cc2c778d717b85d711a76da9
gedeon <gedeonmutikanga@gmail.com>
2024-06-17 21:10:04 +0200

    mise en place des routes pour les composants principals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PublicComponent } from './public/public.component';
+import { UserComponent } from './user/user.component';
+import { AuthComponent } from './auth/auth.component';
+import path from 'node:path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'public', pathMatch: 'full' },
+  { path: 'public', component: PublicComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'auth', component: AuthComponent },
+];
