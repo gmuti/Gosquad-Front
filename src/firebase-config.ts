d62803c391d40e56b198957fb19468358c939a96
gedeon <gedeonmutikanga@gmail.com>
2024-06-20 19:52:32 +0200

    Retire firebase-config.ts de l'index et l'ajoute au .gitignore et page public

--- src/firebase-config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCzk8_9Th7qPsADjJp8KV_IwVHpyvdtiNs',
-  authDomain: 'projet-gosquad.firebaseapp.com',
-  projectId: 'projet-gosquad',
-  storageBucket: 'projet-gosquad.appspot.com',
-  messagingSenderId: '193562883407',
-  appId: '1:193562883407:web:4c657f5ec8c3040678813a',
-  measurementId: 'G-X3RX603MKD',
-};
