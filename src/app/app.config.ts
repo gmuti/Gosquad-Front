2e7c9df44e2e3f2319eb012331f28eca2782f9de
gedeon <gedeonmutikanga@gmail.com>
2024-06-20 19:55:27 +0200

    modif page public

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,20 @@
+// src/app/app-config.ts
 import { ApplicationConfig } from '@angular/core';
-import { RouterModule, provideRouter } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 import { firebaseConfig } from '../firebase-config';
-import { FormsModule } from '@angular/forms';
+import { importProvidersFrom } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    FormsModule,
-    RouterModule,
     provideRouter(routes),
     provideClientHydration(),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideAuth(() => getAuth()),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideAuth(() => getAuth())
+    ),
   ],
 };
